4e374c40b63d30aac8ba2310a8c7188b677fddbe
Victor Coron <103970493+cocaicara@users.noreply.github.com>
2022-07-01 14:51:52 -0300

    Game novo

---
+++ script.js
@@ -0,0 +1,37 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+console.log(mario);
+
+const jump = () => {
+    mario.classList.add('jump')
+
+
+    setTimeout(() => { 
+        mario.classList.remove('jump')
+    }, 500)
+}
+
+const loop = setInterval(() => {
+    const pipePos = pipe.offsetLeft
+    const marioPos = +window.getComputedStyle(mario).bottom.replace('px', '')
+    const over = document.querySelector('h1#fonte')
+
+    if(pipePos <= 120 && pipePos > 0 && marioPos < 80) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePos}px`
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPos}px`;
+
+        mario.src = './imagens2/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+        over.innerHTML = 'Game Over!'
+
+
+
+
+        clearInterval(loop)
+    }
+}, 10)
+document.addEventListener('keydown', jump);
